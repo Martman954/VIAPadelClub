dbd31d28e9e80d202508b2d7e2baa4a9e00911a7
Matej Paľaš <matej.palas954@gmail.com>
2026-06-13 21:50:44 +0200

    Revert "Merge branch 'main' of https://github.com/Martman954/VIAPadelClub"
    This reverts commit 42c524a0a63e3f7451295873db83e2c22be96731, reversing
    changes made to 1c7fe3d221b15c282bfa53dfaf7528e2eea3ed3f.

--- src/Core/VIAPadelClub.Core.Domain/Services/BookingRequest.cs
+++
@@ -1,9 +0,0 @@
-using VIAPadelClub.Core.Domain.Aggregates.Court;
-using VIAPadelClub.Core.Domain.Aggregates.Player;
-using VIAPadelClub.Core.Domain.Aggregates.Schedule;
-using VIAPadelClub.Core.Domain.Common.Values;
-
-namespace VIAPadelClub.Core.Domain.Services;
-
-public record BookingRequest(Player Player, Court Court, Schedule Schedule, TimeInterval TimeInterval);
-

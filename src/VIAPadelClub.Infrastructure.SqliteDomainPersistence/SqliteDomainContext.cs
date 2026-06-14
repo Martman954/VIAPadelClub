0781eb2546275735d8bf2f6726e43f9911602cdb
Mariia Moskovko <mariia.moskovko@lego.com>
2026-06-14 13:08:08 +0200

    repo pattern setup

---
+++ src/VIAPadelClub.Infrastructure.SqliteDomainPersistence/SqliteDomainContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VIAPadelClub.Infrastructure.SqliteDomainPersistence;
+
+public class SqliteDomainContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SqliteDomainContext).Assembly);
+    }
+}
